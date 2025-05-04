21563a53f054b229b41a6a6346dab0b9485754e8
Navya Hegde <navyah0608@gmail.com>
2025-05-04 10:04:24 +0530

    Adding the second project

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+   $fullName = $_POST['fullName'];
+   $emailAddress = $_POST['emailAddress'];
+   $mobileNumber = $_POST['mobileNumber'];
+   $emailSubject = $_POST['emailSubject'];
+   $yourMessage = $_POST['yourMessage'];
+  
+   
+  
+?>
